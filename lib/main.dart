53c5a2ddca8c90d3b56b2824715ef6d94a96199f
Maddi Jitendra <116104814+Jitendra797@users.noreply.github.com>
2025-04-11 10:28:39 +0530

    Refactor main app to use GetMaterialApp and set up themes

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:text_behind_image/home.dart';
+import 'package:text_behind_image/utils/theme/theme.dart';
 
 void main() {
   runApp(const MainApp());
@@ -9,12 +12,12 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
+    return GetMaterialApp(
+      debugShowCheckedModeBanner: false,
+      themeMode: ThemeMode.system,
+      theme: KAppTheme.lightTheme,
+      darkTheme: KAppTheme.darkTheme,
+      home: const HomePage(),
     );
   }
 }
