745ef878c64ff9ab9b3249a4af485e2e733bd3c6
Maddi Jitendra <116104814+Jitendra797@users.noreply.github.com>
2025-04-11 11:31:49 +0530

    Refactor HomePage layout and styling for improved readability and consistency

--- lib/home.dart
+++ lib/home.dart
@@ -1,38 +1,45 @@
 import 'package:flutter/material.dart';
+import 'package:text_behind_image/utils/constants/sizes.dart';
+import 'package:text_behind_image/utils/helpers/helper_functions.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final isDark = KHelperFunctions.isDarkMode(context);
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Home Page'),
-      ),
       body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 24),
+        padding: const EdgeInsets.symmetric(horizontal: KSizes.sm),
         child: Column(
             crossAxisAlignment: CrossAxisAlignment.center,
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              Row(
-                children: [
-                  Expanded(
-                    child: Text(
-                      softWrap: true,
-                      overflow: TextOverflow.visible,
-                      maxLines: 2,
-                      textAlign: TextAlign.center,
-                      'Welcome, get started by uploading an image!',
-                      style: TextStyle(
-                        fontSize: 20,
-                        fontWeight: FontWeight.w400,
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: KSizes.md),
+                child: Row(
+                  children: [
+                    Expanded(
+                      child: Text(
+                        softWrap: true,
+                        overflow: TextOverflow.visible,
+                        maxLines: 2,
+                        textAlign: TextAlign.center,
+                        'Welcome, get started by uploading an image!',
+                        style: TextStyle(
+                          fontSize: KSizes.fontSizeMd,
+                          fontWeight: KSizes.fontWeightMd,
+                        ),
                       ),
                     ),
-                  ),
-                ],
+                  ],
+                ),
               ),
             ]),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: const Icon(Icons.add_photo_alternate_outlined),
       ),
     );
   }
